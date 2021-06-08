e9b56c2287b88e448d49dd57271124e7560f2ea5
krish <72214033+krishzuz@users.noreply.github.com>
2021-06-08 20:20:39 +0530

    Update send.js

--- server/send.js
+++ server/send.js
@@ -1,10 +1,8 @@
 var admin = require('firebase-admin');
 
-var serviceAccount = require('../server/project-9fbe4-firebase-adminsdk-72fqr-5b7791a67f.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://project-9fbe4-default-rtdb.firebaseio.com',
+  
 });
 
 var registrationToken = [
